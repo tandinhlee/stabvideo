9aeec8a018d72d3c6cd45817fd113feb2326e195
Dinh Le <tandinhlee@gmail.com>
2019-01-17 17:43:10 -0600

    Initial Commit

---
+++ stabvideo/ViewController.swift
@@ -0,0 +1,20 @@
+//
+//  ViewController.swift
+//  stabvideo
+//
+//  Created by Dinh Le on 1/17/19.
+//  Copyright © 2019 Dinh Le. All rights reserved.
+//
+
+import UIKit
+
+class ViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+        // Do any additional setup after loading the view, typically from a nib.
+    }
+
+
+}
+
