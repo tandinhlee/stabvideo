2e8c431858b8d88b8c1fd6a5c4bd28f94e61ff4b
Dinh Le <tandinhlee@gmail.com>
2019-01-17 17:58:00 -0600

    Init project
    Init project stabilization video for iOS

---
+++ Podfile
@@ -0,0 +1,20 @@
+# Uncomment the next line to define a global platform for your project
+# platform :ios, '9.0'
+
+target 'stabvideo' do
+  # Comment the next line if you're not using Swift and don't want to use dynamic frameworks
+  use_frameworks!
+
+  # Pods for stabvideo
+
+  target 'stabvideoTests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+  target 'stabvideoUITests' do
+    inherit! :search_paths
+    # Pods for testing
+  end
+
+end
---
+++ stabvideo/EditorViewController.swift
@@ -0,0 +1,30 @@
+//
+//  EditorViewController.swift
+//  stabvideo
+//
+//  Created by Dinh Le on 1/17/19.
+//  Copyright © 2019 Dinh Le. All rights reserved.
+//
+
+import UIKit
+
+class EditorViewController: UIViewController {
+
+    override func viewDidLoad() {
+        super.viewDidLoad()
+
+        // Do any additional setup after loading the view.
+    }
+    
+
+    /*
+    // MARK: - Navigation
+
+    // In a storyboard-based application, you will often want to do a little preparation before navigation
+    override func prepare(for segue: UIStoryboardSegue, sender: Any?) {
+        // Get the new view controller using segue.destination.
+        // Pass the selected object to the new view controller.
+    }
+    */
+
+}
